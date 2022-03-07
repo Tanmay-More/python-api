1dcc7791e2fc52cd5c3c117f227abfcdc97dd5c2
Tanmay-More <tanmay.more@avilasinfotech.in>
2022-03-07 11:39:43 +0530

    change port from 8000 to 5000

--- app.py
+++ app.py
@@ -70,6 +70,6 @@ def emp_del():
 	return "200 OK - Record deleted successfully."
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=8000, debug=True)
+	app.run(host='0.0.0.0', port=5000, debug=True)
 
 
